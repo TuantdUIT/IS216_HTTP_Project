d9cc79f7afe33bceabbe2a2952636276bc0a1593
huy <oca140805@gmail.com>
2025-05-24 22:42:56 +0700

    Huy_Dashboard_Client

--- hotelmanagement/src/hotelmanagement/entity/dba_connection.java
+++ hotelmanagement/src/hotelmanagement/entity/dba_connection.java
@@ -3,11 +3,13 @@ package hotelmanagement.entity;
 
 
 public class dba_connection {
+    public String driver;
     public String url;
     public String username;
     public String password;
 
     public dba_connection() {
+        driver = "oracle.jdbc.OracleDriver";
         url = "jdbc:oracle:thin:@localhost:1521:orcltdt";
         username = "java01";
         password = "java01";
